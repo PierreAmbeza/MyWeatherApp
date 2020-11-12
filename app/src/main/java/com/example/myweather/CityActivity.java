3b5a6f447d82e42976dc7704a0afd128085ef47c
PierreAmbeza <pierre.ambeza@efrei.net>
2020-11-12 09:12:12 +0100

    Problème affichage Recycler réglé

--- app/src/main/java/com/example/myweather/CityActivity.java
+++ app/src/main/java/com/example/myweather/CityActivity.java
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import com.example.myweather.bo.City;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.RecyclerView;
 import com.example.myweather.repository.CityRepository;
@@ -19,24 +21,22 @@ public class CityActivity extends AppCompatActivity implements OnClickListener {
 
     private RecyclerView recyclerView;
 
-    public static final String TAG = CityActivity.class.getSimpleName();
+    private View emptyView;
 
+    public static final String TAG = CityActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        final List<City> cities = CityRepository.getInstance(this).getCities();
-        if(cities.isEmpty())
-        {
-            setContentView(R.layout.activity_city_empty);
-        }
-        else {
-            setContentView(R.layout.activity_city);
-            recyclerView = findViewById(R.id.recyclerView);
-            recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
-        }
-        //We configure the click on the fab
+        setContentView(R.layout.activity_city);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+
+        emptyView = findViewById(R.id.empty_view);
         findViewById(R.id.fab).setOnClickListener(this);
+        setup();
+
     }
 
     @Override
@@ -45,21 +45,39 @@ public class CityActivity extends AppCompatActivity implements OnClickListener {
         super.onResume();
         //We init the list into the onResume method
         //so the list is updated each time the screen goes to foreground
-        initList();
+        //initList();
+        setup();
     }
 
+
+    private void setup() {
+        final List<City> cities = CityRepository.getInstance(this).getCities();
+        final CitiesAdapter citiesAdapter = new CitiesAdapter(cities);
+        recyclerView.setAdapter(citiesAdapter);
+        checkIsEmpty(citiesAdapter);
+    }
+
+    /*
     private void initList(){
         //We retrieve the list of cities to display
         final List<City> cities = CityRepository.getInstance(this).getCities();
+        final CitiesAdapter citiesAdapter = new CitiesAdapter(cities);
         //We create the adapter and we attach it to the RecyclerView
-        if(cities.isEmpty()) {
+        if(cities.isEmpty())
+        {
             setContentView(R.layout.activity_city_empty);
             findViewById(R.id.fab).setOnClickListener(this);
         }
-        else {
-            final CitiesAdapter citiesAdapter = new CitiesAdapter(cities);
+        if(citiesAdapter != null)
+        {
             recyclerView.setAdapter(citiesAdapter);
         }
+    }*/
+
+    private void checkIsEmpty(CitiesAdapter citiesAdapter)
+    {
+        emptyView.setVisibility(citiesAdapter == null || citiesAdapter.getItemCount() == 0? View.VISIBLE : View.GONE);
+        recyclerView.setVisibility(citiesAdapter == null || citiesAdapter.getItemCount() == 0? View.GONE : View.VISIBLE);
     }
 
     @Override
